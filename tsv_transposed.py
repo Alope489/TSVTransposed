69c8811e33ea97c064e1b29e2c78c6a72a7f7c40
Alope489 <114271327+Alope489@users.noreply.github.com>
2023-05-19 12:52:06 -0400

    Create tsv_transposed.py
    this is the revised version of xlsx_to_csv

---
+++ tsv_transposed.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import sys
+import os
+
+def tsv_transposed(file_path):
+
+    df = pd.read_excel(file_path)
+    
+    df.replace('_', '|', regex=True, inplace=True)
+
+    df_transposed = df.transpose()
+
+    base_file_name = os.path.basename(file_path).split('.')[0]
+
+    output_file_name = f"{base_file_name}_transposed.tsv"
+
+    df_transposed.to_csv(output_file_name, sep='\t', index=False)
+
+    print(f"Transposed file has been saved as {output_file_name}")
+    
+    #needs the reformatting code for the header lines and the name.
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python tsv_transposed.py <file_path>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+
+    tsv_transposed(file_path)
